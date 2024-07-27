3445fc122b698edf9f77925d71f09ee84e3b91ab
tao0024 <tawu0024@foxmail.com>
2024-07-27 00:52:05 -0700

    fix the ESLint warning

--- src/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
 import { useSelector } from "react-redux";
--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -9,7 +9,6 @@ import ParentStateComponent from "./ParentStateComponent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
 import ReduxExamples from "./ReduxExamples";
-import HelloReducer from "./ReduxExamples/HelloRedux/helloReducer";
 import StringStateVariables from "./StringStateVariables";
 
 export default function Lab4() {
